09a29da953deadee6e68f593e84f92ede46d7d2f
John Michael McCasland <jmmccasland@gmail.com>
2017-02-28 08:34:22 -0600

    Begin testing

--- tests/reducer-test.js
+++ tests/reducer-test.js
@@ -1,7 +1,24 @@
 import reducer from '../app/reducer';
 
+
+const defaultState = Object.freeze({
+  games: [],
+});
+
+const contactOne = Object.freeze({
+  _id: '1234',
+  firstname: 'John',
+  lastname: 'McCasland',
+  street: '123 Ivy St',
+  state: 'TN',
+});
+
 module('reducer', () => {
   test('it exists', (assert) => {
     assert.ok(reducer, 'reducer exists');
+  });
+
+  test('it has a default state', (assert) => {
+    assert.deepEqual(reducer(null, {}), defaultState, 'Returns the default state');
   });
 });
